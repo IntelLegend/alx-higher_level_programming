278e827b80d03055ed0fcfbca0c61c0316eeebbc
intellegend <odudujohn@yahoo.com>
2022-12-07 14:29:21 -0800

    cool tasks

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
+
+
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
-    return a_dictionary
+    if a_dictionary is not None:
+        new_dict = a_dictionary
+        new_dict[key] = value
+        return new_dict
 
 if __name__ == '__main__':
-    u = update_dictionary
-    p = __import__('6-print_sorted_dictionary').print_sorted_dictionary
+    print_s = __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
     a_dictionary = {'language': "C", 'number': 89, 'track': "Low level"}
-    new_dict = u(a_dictionary, 'language', "Python")
-    p(new_dict)
+    new_dict = update_dictionary(a_dictionary, 'language', "Python")
+    print_s(new_dict)
     print("--")
-    p(a_dictionary)
+    print_s(a_dictionary)
 
     print("--")
     print("--")
 
-    new_dict = u(a_dictionary, 'city', "San Francisco")
-    p(new_dict)
+    new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
+    print_s(new_dict)
     print("--")
-    p(a_dictionary)
+    print_s(a_dictionary)
